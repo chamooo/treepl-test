da549d8b215409a5fe4d77416bac8a06cee35c38
Roman Melnychuk <roman4iek02@gmail.com>
2023-05-23 12:05:54 +0300

    first commit

---
+++ dist/assets/js/main.js
@@ -0,0 +1,61 @@
+window.addEventListener("DOMContentLoaded", function () {
+
+    // header ------------------------------------------------
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+    const links = document.querySelectorAll(".nav-links li");
+    const menu = document.querySelector('.header__menu');
+    hamburger.addEventListener('click', ()=>{
+        navLinks.classList.toggle("open");
+        menu.classList.toggle("open");
+        links.forEach(link => {
+            link.classList.toggle("fade");
+        });
+        hamburger.classList.toggle("toggle");
+    });
+
+
+    // anchors -------------------------------------------------
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // scroll animations ----------------------------------------
+    const animItems = document.querySelectorAll('._anim-item');
+
+    if(animItems.length > 0) {
+        document.addEventListener('scroll', animOnScroll);
+    }
+
+    function animOnScroll() {
+        animItems.forEach(animItem => {
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 1.5;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if((pageYOffset > animItemOffset - animItemPoint) &&
+                pageYOffset < (animItemOffset + animItemHeight)) {
+                let time = setTimeout(() => {
+                    animItem.classList.add('_active-animation');
+                }, 0);
+            }
+        });
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+    }
+});
